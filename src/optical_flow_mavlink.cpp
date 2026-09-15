10bdd11b1cae263f548d5edf3eaec323a3e10ec9
Mikail Stolpovskikh <monkeysbrain@yandex.ru>
2026-09-15 17:21:22 +0300

    feat: apply proven lever-arm compensation to production flow

--- src/optical_flow_mavlink.cpp
+++ src/optical_flow_mavlink.cpp
@@ -752,7 +752,7 @@ struct FlowStep {
 
   // Lever-arm shadow: convert optical flow measured at the displaced camera
   // focal point to the FC/IMU reference point using v_cam = omega x r.
-  // Diagnostic only: NEVER sent to FC until a yaw regression proves it.
+  // Proven by repeated bench yaw regression; used for production send when valid.
   bool lever_shadow_valid=false;
   double lever_flow_body_x=0.0,lever_flow_body_y=0.0;
   double lever_pred_flow_x=0.0,lever_pred_flow_y=0.0;
@@ -1461,7 +1461,7 @@ int main(int argc,char** argv){
     constexpr std::streamoff kCsvMaxBytes=250LL*1024LL*1024LL;
     bool csv_logging_enabled=true;
     bool csv_limit_reported=false;
-    csv<<"mono_ns,camera_ts_ns,v4l2_timestamp_ns,camera_dequeue_ns,v4l2_flags,v4l2_to_dequeue_ms,flow_send_ns,frame_pipeline_latency_ms,camera_queue_dropped,camera_queue_dropped_total,frame,guide_leg,guide_stage,valid,invalid_reason,bridge_pending,dt_s,features,tracked,inliers,inlier_ratio,t_features_ms,t_lk_ms,t_ransac_ms,t_post_ms,du_px,dv_px,du_norm,dv_norm,yaw_rate_cam_z,scale_rate,lk_height_scale,flow_cam_x,flow_cam_y,flow_body_x,flow_body_y,dr_valid,dr_flow_body_x,dr_flow_body_y,dr_scale_rate,dr_interp_gap_ms,lever_valid,lever_flow_body_x,lever_flow_body_y,lever_pred_flow_x,lever_pred_flow_y,lever_aligned_valid,lever_aligned_flow_body_x,lever_aligned_flow_body_y,lever_aligned_pred_flow_x,lever_aligned_pred_flow_y,lever_aligned_gyro_x,lever_aligned_gyro_y,lever_aligned_gyro_z,lever_aligned_gap_ms,ab_fb_enabled,ab_fb_max_px,ab_fb_checked,ab_fb_pass,ab_fb_ratio,ab_fb_inliers,ab_fb_valid,ab_fb_flow_body_x,ab_fb_flow_body_y,ab_fb_t_ms,ab_robust_valid,ab_robust_flow_body_x,ab_robust_flow_body_y,ab_robust_sigma,ab_robust_mean_weight,ab_robust_downweighted,ab_robust_iters,ab_obs_valid,ab_obs_flow_body_x,ab_obs_flow_body_y,ab_obs_median_ratio,ab_obs_mean_weight,ab_obs_downweighted,quality,luna_m,luna_age_ms,range_to_fc_m,flow_send_x,flow_send_y,flow_sent,range_sent,fc_armed,ekf_local_valid,ekf_x_ned,ekf_y_ned,ekf_z_ned,ekf_vx_ned,ekf_vy_ned,ekf_vz_ned,ekf_age_ms,ekf_count,ekf_status_valid,ekf_flags,ekf_status_age_ms,ekf_status_count,ekf_vel_var,ekf_pos_h_var,ekf_pos_v_var,ekf_compass_var,ekf_terrain_var,return_event,fc_roll,fc_pitch,fc_yaw,fc_gyro_x,fc_gyro_y,fc_gyro_z,fc_gyro_age_ms,fc_gyro_samples,ctrl_target_valid,ctrl_target_x,ctrl_target_y,ctrl_target_vx,ctrl_target_vy,ctrl_target_age_ms,att_target_valid,att_target_roll,att_target_pitch,att_target_yaw,att_target_thrust,att_target_age_ms,outputs_valid,out1,out2,out3,out4,out5,out6,out7,out8,outputs_age_ms,c0_n,c0_bx,c0_by,c1_n,c1_bx,c1_by,c2_n,c2_bx,c2_by,c3_n,c3_bx,c3_by,c4_n,c4_bx,c4_by,c5_n,c5_bx,c5_by,c6_n,c6_bx,c6_by,c7_n,c7_bx,c7_by,c8_n,c8_bx,c8_by\n";
+    csv<<"mono_ns,camera_ts_ns,v4l2_timestamp_ns,camera_dequeue_ns,v4l2_flags,v4l2_to_dequeue_ms,flow_send_ns,frame_pipeline_latency_ms,camera_queue_dropped,camera_queue_dropped_total,frame,guide_leg,guide_stage,valid,invalid_reason,bridge_pending,dt_s,features,tracked,inliers,inlier_ratio,t_features_ms,t_lk_ms,t_ransac_ms,t_post_ms,du_px,dv_px,du_norm,dv_norm,yaw_rate_cam_z,scale_rate,lk_height_scale,flow_cam_x,flow_cam_y,flow_body_x,flow_body_y,dr_valid,dr_flow_body_x,dr_flow_body_y,dr_scale_rate,dr_interp_gap_ms,lever_valid,lever_production_applied,lever_flow_body_x,lever_flow_body_y,lever_pred_flow_x,lever_pred_flow_y,lever_aligned_valid,lever_aligned_flow_body_x,lever_aligned_flow_body_y,lever_aligned_pred_flow_x,lever_aligned_pred_flow_y,lever_aligned_gyro_x,lever_aligned_gyro_y,lever_aligned_gyro_z,lever_aligned_gap_ms,ab_fb_enabled,ab_fb_max_px,ab_fb_checked,ab_fb_pass,ab_fb_ratio,ab_fb_inliers,ab_fb_valid,ab_fb_flow_body_x,ab_fb_flow_body_y,ab_fb_t_ms,ab_robust_valid,ab_robust_flow_body_x,ab_robust_flow_body_y,ab_robust_sigma,ab_robust_mean_weight,ab_robust_downweighted,ab_robust_iters,ab_obs_valid,ab_obs_flow_body_x,ab_obs_flow_body_y,ab_obs_median_ratio,ab_obs_mean_weight,ab_obs_downweighted,quality,luna_m,luna_age_ms,range_to_fc_m,flow_send_x,flow_send_y,flow_sent,range_sent,fc_armed,ekf_local_valid,ekf_x_ned,ekf_y_ned,ekf_z_ned,ekf_vx_ned,ekf_vy_ned,ekf_vz_ned,ekf_age_ms,ekf_count,ekf_status_valid,ekf_flags,ekf_status_age_ms,ekf_status_count,ekf_vel_var,ekf_pos_h_var,ekf_pos_v_var,ekf_compass_var,ekf_terrain_var,return_event,fc_roll,fc_pitch,fc_yaw,fc_gyro_x,fc_gyro_y,fc_gyro_z,fc_gyro_age_ms,fc_gyro_samples,ctrl_target_valid,ctrl_target_x,ctrl_target_y,ctrl_target_vx,ctrl_target_vy,ctrl_target_age_ms,att_target_valid,att_target_roll,att_target_pitch,att_target_yaw,att_target_thrust,att_target_age_ms,outputs_valid,out1,out2,out3,out4,out5,out6,out7,out8,outputs_age_ms,c0_n,c0_bx,c0_by,c1_n,c1_bx,c1_by,c2_n,c2_bx,c2_by,c3_n,c3_bx,c3_by,c4_n,c4_bx,c4_by,c5_n,c5_bx,c5_by,c6_n,c6_bx,c6_by,c7_n,c7_bx,c7_by,c8_n,c8_bx,c8_by\n";
 
     if(g_fb_shadow_max_px>0.0){
       std::cerr<<(g_obs_shadow_enabled?"A/B/C/D SHADOW: ":"A/B/C SHADOW: ")
@@ -1923,7 +1923,13 @@ int main(int argc,char** argv){
         }
 
         bool flow_sent=false; uint8_t quality=0;
-        double flow_send_x=s.flow_body_x, flow_send_y=s.flow_body_y;
+        // Production lever-arm compensation.  The camera focal point has real
+        // linear velocity omega x r when the rigid body rotates about the FC/IMU.
+        // Remove only that translation-like optical-flow component.  If the
+        // shadow cannot be computed, preserve the proven pre-change flow path.
+        const bool lever_production_applied=s.valid && s.lever_shadow_valid;
+        double flow_send_x=lever_production_applied ? s.lever_flow_body_x : s.flow_body_x;
+        double flow_send_y=lever_production_applied ? s.lever_flow_body_y : s.flow_body_y;
         if(s.valid && bench_height_override>0.0){
           double real_camera_height=0.0;
           double fake_camera_height=bench_height_override;
@@ -1955,8 +1961,8 @@ int main(int argc,char** argv){
             //
             // This makes AP's post-compensation residual k*translation, which
             // paired with the synthetic range preserves the real metric speed.
-            flow_send_x = fg.x + k*(s.flow_body_x - fg.x);
-            flow_send_y = fg.y + k*(s.flow_body_y - fg.y);
+            flow_send_x = fg.x + k*(flow_send_x - fg.x);
+            flow_send_y = fg.y + k*(flow_send_y - fg.y);
           }
         }
         int64_t flow_send_ns=monoNs();
@@ -2110,7 +2116,7 @@ int main(int argc,char** argv){
            <<s.scale_rate<<','<<s.lk_height_scale<<','
            <<s.flow_cam_x<<','<<s.flow_cam_y<<','<<s.flow_body_x<<','<<s.flow_body_y<<','
            <<(s.dr_shadow_valid?1:0)<<','<<s.dr_flow_body_x<<','<<s.dr_flow_body_y<<','<<s.dr_scale_rate<<','<<s.dr_interp_gap_ms<<','
-           <<(s.lever_shadow_valid?1:0)<<','<<s.lever_flow_body_x<<','<<s.lever_flow_body_y<<','
+           <<(s.lever_shadow_valid?1:0)<<','<<(lever_production_applied?1:0)<<','<<s.lever_flow_body_x<<','<<s.lever_flow_body_y<<','
            <<s.lever_pred_flow_x<<','<<s.lever_pred_flow_y<<','
            <<(s.lever_aligned_valid?1:0)<<','<<s.lever_aligned_flow_body_x<<','<<s.lever_aligned_flow_body_y<<','
            <<s.lever_aligned_pred_flow_x<<','<<s.lever_aligned_pred_flow_y<<','
